072bf45a552331a3ad5066637782e03061cc9030
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2018-11-19 09:34:52 +0600

    DFS final 1

---
+++ Offline 1 - Cannibal Missionary Problem/Cannibal Missionary Problem/src/cannibal/missionary/problem/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cannibal.missionary.problem;
+
+/**
+ *
+ * @author Heisenberg
+ */
+public class Constants {
+    static int LEFT = 0; 
+    static int RIGHT = 1; 
+    static boolean visited = true; 
+    static boolean notVisited = false; 
+}
